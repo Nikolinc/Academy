59f23539b08ca823502ebc07a2def11d8d8765e0
Nikolinc <tsigulskynikita@gmail.com>
2023-10-30 06:33:54 +0500

    fix error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import Vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [react(), Vue()],
+  plugins: [react()],
 });
