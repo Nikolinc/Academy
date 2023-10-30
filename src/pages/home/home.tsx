d1b20c46b804bb4c7711e1b8f3e55b9a4faf2fc0
Nikolinc <tsigulskynikita@gmail.com>
2023-10-30 05:51:37 +0500

    add sound list

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,20 @@
+import SoundList from '../../component/soundList/soundList';
+import { SoundSelector } from '../../store/slices/SoundSlices';
+import { ISoundList, category } from '../../types/soundlist';
 import './home.css'
+import { useSelector } from 'react-redux';
 
 function Home() {
-  return <div className="home"></div>
+  const soundList: ISoundList[] = useSelector(SoundSelector.getSoundList);
+
+  const fNewPlaylist = (sound: ISoundList, category: category) => {
+    return sound.category.indexOf(category) !== -1
+  }
+
+  return <div className="home">
+    <SoundList soundList={soundList.filter((sound) => fNewPlaylist(sound, category.NewPlaylist))} />
+    <SoundList soundList={soundList.filter((sound) => fNewPlaylist(sound, category.Trending))} />
+  </div>
 }
 
 export default Home
