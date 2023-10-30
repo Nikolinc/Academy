212af606f3ed9cf1e2487d389cd031b3901de863
Nikolinc <tsigulskynikita@gmail.com>
2023-10-30 05:43:59 +0500

    add header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,18 +2,22 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from '../pages/home/home'
 import LeftBar from '../component/leftBar/leftBar'
-
+import Header from '../component/header/header'
+import Sound from '../pages/sound/sound'
 function App() {
-
 
   return (
     <div className="App">
       <LeftBar />
-      <Routes>
-        <Route path="/tasks/:id" element={<></>} />
-        <Route path="/home" element={<Home />} />
-        <Route index element={<Home />} />
-      </Routes>
+      <div className='page'>
+        <Header />
+        <Routes>
+          <Route path="/sound/:id" element={<Sound />} />
+          <Route path="/home" element={<Home />} />
+          <Route index element={<Home />} />
+        </Routes>
+      </div>
+
     </div>
   )
 }
